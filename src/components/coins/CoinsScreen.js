ab9b29f983b0d89c7114102cf8a2eae2357b6a5e
Pablo <pablogarcirodriguez@gmail.com>
2022-10-09 11:22:34 +0200

    Primera pantalla con Stack Navigator

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,16 @@
+import React, { Component} from "react";
+import {View, Text} from "react-native";
+
+class CoinsScreen extends Component {
+
+    render(){
+        return(
+            <View>
+                <Text>Coins Screen</Text>
+            </View>
+
+        )
+    }
+}
+
+export default CoinsScreen;
